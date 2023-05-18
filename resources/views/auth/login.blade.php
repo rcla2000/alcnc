d459ccd16877c7aa3d4180cf87a393ff0101e19e
Roberto Carlos López Abarca <rcla200031@gmail.com>
2023-05-18 13:30:16 -0600

    Vista de login actualizada

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,69 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+<!DOCTYPE html>
+<html lang="es">
 
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Nuevo Cuscatlán, La Nueva ciudad">
+    <meta name="keywords" content="Alcaldía, Nuevo Cuscatlán, Nuevas ideas">
+    <meta name="author" content="Luis H. Medrano">
+    <link rel="shortcut icon" href="{{ asset('img/logo.png') }}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('img/logo68.png') }}" type="image/png" style="width: 50%;">
+    <title>Alcaldía Nuevo Cuscatlán | Login</title>
+    @include('layouts.master.style')
+</head>
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+<body>
+    <div class="contenedor-vista-login">
+        <!-- Material form login -->
+        <div class="contenedor-login">
+            <div class="card">
+                <div class="card-header white-text text-center py-4 d-flex justify-content-between align-items-center">
+                    <strong class="ml-3 titulo-login">Iniciar sesión</strong>
+                    <img src="{{ asset('img/logo.svg') }}" class="logoSvg" alt="La-Nueva-Ciudad">
+                </div>
+                <!--Card content-->
+                <div class="card-body px-lg-5 pt-0">
+                    <!-- Form -->
+                    <form method="POST" class="text-center" style="color: #757575;" action="{{ route('login') }}">
+                        @csrf
+                        <!-- Email -->
+                        <div class="md-form">
+                            <input 
+                                type="email" 
+                                id="email" 
+                                name="email" 
+                                class="form-control @error('email') is-invalid @enderror" 
+                                value="{{ old('email') }}" autofocus>
+                            <label for="email">Correo electrónico: </label>
+                            @error('email')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+                        <!-- Password -->
+                        <div class="md-form">
+                            <input type="password" id="password" name="password" class="form-control">
+                            <label for="password">Contraseña: </label>
+                        </div>
 
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                        <!-- Sign in button -->
+                        <button class="btn btn-primary btn-block"
+                            type="submit">
+                            <i class="fas fa-sign-in-alt mr-2"></i>
+                            Acceder
+                        </button>
+                    </form>
+                    <!-- Form -->
+                </div>
             </div>
+        </div>
+        <!-- Material form login -->
+    </div>
+    @include('layouts.master.script')
+</body>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
-            </div>
-
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+</html>
