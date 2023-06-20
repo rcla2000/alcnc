ac6c222bb8f38bd0036492567e3807649834ee0e
= <=>
2023-06-20 14:32:26 -0600

    Agregando funcionalidad de envio de mensajes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\CServicio;
+use App\Models\Mensaje;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class HomeController extends Controller
 {
@@ -15,5 +17,22 @@ class HomeController extends Controller
     function documentos(){
         $servicios = CServicio::all();
         return view('descargables', compact('servicios'));
+    }
+
+    function guardarMensaje(Request $request) {
+        $request->validate([
+            'nombre' => 'required|string|min:3',
+            'telefono' => 'required|string',
+            'mensaje' => 'required|string'
+        ]);
+
+        $mensaje = new Mensaje();
+        $mensaje->nombre = $request->nombre;
+        $mensaje->telefono = $request->telefono;
+        $mensaje->mensaje = $request->mensaje;
+        $mensaje->save();
+
+        Alert::success('Información', 'Su mensaje ha sido enviado');
+        return back();
     }
 }
--- resources/views/layouts/master/style.blade.php
+++ resources/views/layouts/master/style.blade.php
@@ -22,4 +22,5 @@
     rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
   />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.css">
 @yield('styles')
